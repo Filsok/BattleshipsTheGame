43508848a08a4c8cbef036f6813f6058b3b6a387
Filip Sokolowski <filippawelsokolowski@gmail.com>
2022-08-21 20:51:56 +0200

    SetupGame() finished:

--- BattleshipsTheGame/BattleshipsWindow.xaml.cs
+++ BattleshipsTheGame/BattleshipsWindow.xaml.cs
@@ -18,71 +18,59 @@ namespace BattleshipsTheGame
     public partial class BattleshipsWindow : Window
     {
         private int _battlefeldSize = 10;
-
+        private int _pointsCounter = 0;
+        private int _movesCounter = 0;
+        private bool[,]? _battlefieldArray;
+        private List<int>? _shipsList;
 
         public BattleshipsWindow()
         {
             InitializeComponent();
-            SetUpGame();
+            SetupGame();
         }
 
-
-        private void SetUpGame()
+        private void SetupGame()
         {
-            List<Ship> shipsList = new List<Ship>()
-            {
-                new Ship("Battleship",5),
-                new Ship("Destroyer",4),
-                new Ship("Destroyer",4)
-            };
-
-            bool[,] battlefieldArray = new bool[_battlefeldSize, _battlefeldSize];
-
-            //"💥"
-            //⭕❌＞︿＜(˘･_･˘)(ㆆ_ㆆ)✕💢💥
+            _shipsList?.Clear();
+            _shipsList = new List<int>() { 5, 4, 4 };
+            _battlefieldArray = new bool[_battlefeldSize, _battlefeldSize];
 
             var random = new Random();
             int direction, column, row;
             bool isEnoughSpace;
-            List<KeyValuePair<int,int>> tmpShipPosition = new List<KeyValuePair<int,int>>();
-            foreach (Ship ship in shipsList)
+            List<KeyValuePair<int, int>> tmpShipPosition = new List<KeyValuePair<int, int>>();
+            foreach (int ship in _shipsList)
             {
                 isEnoughSpace = false;
                 while (!isEnoughSpace)
                 {
                     tmpShipPosition.Clear();
                     //draw a rotate of this ship
-                    direction = random.Next(0, 3);               //directions: 0-up, 1-down, 2-right, 3-left
+                    direction = random.Next(0, 2);               //directions: 0-vertical(down), 1-horizontal(right)
 
                     //draw a start point on the battlefield
-                    if (direction < 2)
+                    if (direction == 0)
                     {
-                        row = direction == 1 ? random.Next(1 + ship.Length, _battlefeldSize) : random.Next(1, _battlefeldSize - ship.Length);
+                        row = random.Next(1, _battlefeldSize - ship);
                         column = random.Next(1, _battlefeldSize);
                     }
                     else
                     {
-                        column = direction == 3 ? random.Next(1 + ship.Length, _battlefeldSize) : random.Next(1, _battlefeldSize - ship.Length);
+                        column = random.Next(1, _battlefeldSize - ship);
                         row = random.Next(1, _battlefeldSize);
                     }
 
                     //prepare list of potential fields occupied by the ship
-                    tmpShipPosition.Add(new KeyValuePair<int,int>(row, column));
-                    for (int i = 1; i < ship.Length; i++)
+                    tmpShipPosition.Add(new KeyValuePair<int, int>(row, column));
+                    for (int i = 1; i < ship; i++)
                     {
                         switch (direction)
                         {
-                            case 0:             //up
+                            case 0:             //vertical
                                 tmpShipPosition.Add(new KeyValuePair<int, int>(row + i, column));
                                 break;
-                            case 1:             //down
-                                tmpShipPosition.Add(new KeyValuePair<int, int>(row - i, column));
-                                break;
-                            case 2:             //right
-                                tmpShipPosition.Add(new KeyValuePair<int, int>(row, column+i));
-                                break;
-                            case 3:             //left
-                                tmpShipPosition.Add(new KeyValuePair<int, int>(row, column-i));
+                            case 1:             //horizontal
+                                tmpShipPosition.Add(new KeyValuePair<int, int>(row, column + i));
                                 break;
                             default:
                                 throw new ArgumentOutOfRangeException("direction value is out of range.");
@@ -92,19 +80,13 @@ namespace BattleshipsTheGame
                     //check if any of fields are occupied already
                     isEnoughSpace = true;
                     foreach (var kvp in tmpShipPosition)
-                    {
-                        if (battlefieldArray[kvp.Key,kvp.Value]==true)
-                            isEnoughSpace=false;
-                    }
+                        if (_battlefieldArray[kvp.Key, kvp.Value] == true)
+                            isEnoughSpace = false;
 
                     //put the ship on the battlefield
                     if (isEnoughSpace)
-                    {
                         foreach (var kvp in tmpShipPosition)
-                        {
-                            battlefieldArray[kvp.Key, kvp.Value] = true;
-                        }
-                    }
+                            _battlefieldArray[kvp.Key, kvp.Value] = true;
                 }
             }
         }
